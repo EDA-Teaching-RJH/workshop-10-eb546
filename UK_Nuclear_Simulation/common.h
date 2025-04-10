2921123959b89ec59dad200759bf5b75ef703afd
eb546 <eb546@kent.ac.uk>
2025-04-10 12:47:10 +0000

    commit

--- UK_Nuclear_Simulation/common.h
+++ UK_Nuclear_Simulation/common.h
@@ -1,27 +1,75 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>  // Added for fd_set and timeval
 #include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/err.h>
 #include <pthread.h>
 
+#define CONTROL_PORT 8080
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+#define KEY_SIZE 32
+#define IV_SIZE 16
+#define MAX_TARGETS 100
+#define LOG_FILE "nuclear_log.txt"
+
+// Message types
+typedef enum {
+    MSG_REGISTER,
+    MSG_INTEL,
+    MSG_LAUNCH_ORDER,
+    MSG_LAUNCH_CONFIRM,
+    MSG_STATUS,
+    MSG_ERROR,
+    MSG_TEST
+} MessageType;
+
+// Intel categories
+typedef enum {
+    INTEL_RADAR,
+    INTEL_SATELLITE,
+    INTEL_SUBMARINE
+} IntelType;
+
+// Target information
 typedef struct {
-    char* data;
-    size_t length;
+    char name[50];
+    double latitude;
+    double longitude;
+    int priority;
+} Target;
+
+// Message structure
+typedef struct {
+    MessageType type;
+    char sender[20];
+    char payload[BUFFER_SIZE - sizeof(MessageType) - sizeof(char[20])];
+    unsigned char iv[IV_SIZE];
+    unsigned char mac[EVP_MAX_MD_SIZE];
+    int mac_len;
 } SecureMessage;
 
-// Message functions
-SecureMessage* create_secure_message(const char* data, size_t length);
-void free_secure_message(SecureMessage* msg);
+// Function prototypes
+void handle_error(const char *msg, bool fatal);
+void init_crypto();
+void cleanup_crypto();
+int encrypt_message(SecureMessage *msg, const unsigned char *key);
+int decrypt_message(SecureMessage *msg, const unsigned char *key);
+int verify_message(SecureMessage *msg, const unsigned char *key);
+void generate_random_key(unsigned char *key, int size);
+void log_message(const char *message);
+void print_hex(const char *label, const unsigned char *data, int len);
 
-// File functions
-int encrypt_file(const char* input_path, const char* output_path, const unsigned char* key);
+#endif // COMMON_H
 
-// Logging functions
-void log_message(const char* message);
-
-// System functions
-void initialize_common();
-void cleanup_common();
-
-#endif
